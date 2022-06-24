d27eea3e8992cf3d83d740730a4e3fb88962d1a3
Miki U <mduryszek@gmail.com>
2022-06-24 18:33:15 +0100

    Class for producing n_folds for cross validation of time series

---
+++ cross_validiation_split.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class BlockTSSplit():
+    def __init__(self, n_splits):
+        self.n_splits = n_splits
+
+    def split(self, df):
+        n_samples = len(df)
+        k_fold_size = n_samples // self.n_splits
+        indices = np.arange(n_samples)
+
+        margin = 0
+        for i in range(self.n_splits):
+            start = i * k_fold_size
+            stop = start + k_fold_size
+            mid = int(0.8 * (stop - start)) + start
+            yield indices[start: mid], indices[mid + margin: stop]
+            
