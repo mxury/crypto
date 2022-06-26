05d7fca9630e6b9719d604df1c6119a736703354
Miki U <mduryszek@gmail.com>
2022-06-26 13:55:32 +0100

    Miscalnous functions

---
+++ misc.py
@@ -0,0 +1,89 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from statsmodels.tsa.stattools import adfuller
+
+def plot_candle_stick(ohlc):
+    fig, ax = plt.subplot((1,1))
+
+    width = .4
+    width2 = .05
+
+    # define colors to use
+    col1 = 'green'
+    col2 = 'red'
+
+    if 'close' not in ohlc.columns:
+        raise ValueError('No columns named close')
+
+    up = ohlc[ohlc.close >= ohlc.open]
+    down = ohlc[ohlc.close < ohlc.open]
+
+    # plot up prices
+    plt.bar(up.index, up.close - up.open, width, bottom=up.open, color=col1)
+    plt.bar(up.index, up.high - up.close, width2, bottom=up.close, color=col1)
+    plt.bar(up.index, up.low - up.open, width2, bottom=up.open, color=col1)
+
+    # plot down prices
+    plt.bar(down.index, down.close - down.open, width, bottom=down.open, color=col2)
+    plt.bar(down.index, down.high - down.open, width2, bottom=down.open, color=col2)
+    plt.bar(down.index, down.low - down.close, width2, bottom=down.close, color=col2)
+
+    # rotate x-axis tick labels
+    plt.xticks(rotation=45, ha='right')
+
+    # display candlestick chart
+
+
+# define function to compute log returns
+def log_return(series, periods=1):
+    return np.log(series).diff(periods=periods)
+
+def plot_periodogram(ts, detrend='linear', ax=None):
+    from scipy.signal import periodogram
+    fs = pd.Timedelta("1Y") / pd.Timedelta("1D")
+    freqencies, spectrum = periodogram(
+        ts,
+        fs=fs,
+        detrend=detrend,
+        window="boxcar",
+        scaling='spectrum',
+    )
+    if ax is None:
+        _, ax = plt.subplots()
+    ax.step(freqencies, spectrum, color="purple")
+    ax.set_xscale("log")
+    ax.set_xticks([1, 2, 4, 6, 12, 26, 52, 104])
+    ax.set_xticklabels(
+        [
+            "Annual (1)",
+            "Semiannual (2)",
+            "Quarterly (4)",
+            "Bimonthly (6)",
+            "Monthly (12)",
+            "Biweekly (26)",
+            "Weekly (52)",
+            "Semiweekly (104)",
+        ],
+        rotation=30,
+    )
+    ax.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
+    ax.set_ylabel("Variance")
+    ax.set_title("Periodogram")
+    return ax
+
+def check_stationarity(series):
+#     partially copied from book by Jason Brownless "Intro to Time Series Forecasting..."
+
+    result = adfuller(series.values)
+
+    print('ADF Statistic: %f' % result[0])
+    print('p-value: %f' % result[1])
+    print('Critical Values:')
+    for key, value in result[4].items():
+        print('\t%s: %.3f' % (key, value))
+
+    if (result[1] <= 0.05) & (result[4]['5%'] > result[0]):
+        print("\u001b[32mStationary\u001b[0m")
+    else:
+        print("\x1b[31mNon-stationary\x1b[0m")
