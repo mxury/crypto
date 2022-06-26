bc85fc5309c68d880607e4a0d8bc7f641c5b827c
Miki U <mduryszek@gmail.com>
2022-06-26 22:31:30 +0100

    changed functionality

--- lstm.py
+++ lstm.py
@@ -1,7 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
 from lstm_data_gen import *
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 
 def lstm_model():
@@ -25,10 +25,9 @@ def lstm_model():
     return model
 
 
-def cv_score(model, cv_folds, df, model_params):
+def cv_score(cv_folds, df, model_params):
     """
-    Evaluates a Neural Network model given a split.
-    :param model: Neural Network model
+    Evaluates my LSTM model given a split.
     :param cv_folds: Cross validation split iterator
     :param df: Dataframe containing the data
     :return:
@@ -36,29 +35,33 @@ def cv_score(model, cv_folds, df, model_params):
     scores = list()
     cv_history = list()
     early_stopping = EarlyStopping(
-        min_delta=0.001,  # minimium amount of change to count as an improvement
-        patience=10,  # how many epochs to wait before stopping
+        monitor="val_loss",
+        min_delta=0.0001,  # minimium amount of change to count as an improvement
+        patience=5,  # how many epochs to wait before stopping
         restore_best_weights=True,
     )
-    model_checkpoint = [
-        keras.callbacks.ModelCheckpoint(
+    model_checkpoint = ModelCheckpoint(
             filepath='home/mikiu/Documents/git_projects/crypto/saved_models/model_{epoch}',
             save_freq='epoch')
-    ]
+
     for train, val in cv_folds.split(df):
         df_train = df.iloc[train]
         df_val = df.iloc[val]
         dataset = dataset_generator(df_train, df_val, seq_length=model_params['seq_length'])
 
         dataset_train = dataset.train()
-        print(f'Fitting fold number {len(cv_history)}')
+        dataset_val = dataset.val()
+
+        print(f'Fitting fold number {len(cv_history)+1}')
+        model = lstm_model()
         history = model.fit(dataset_train, epochs=model_params['epochs'], batch_size=model_params['batch_size'],
-                            callbacks=[early_stopping, model_checkpoint],
+                            callbacks=[early_stopping],
+                            validation_data=dataset_val,
                             )
         cv_history.append(history)
 
-        dataset_val = dataset.val()
         scores.append(model.evaluate(dataset_val))
+        model.save(f'home/mikiu/Documents/git_projects/crypto/saved_models/model_{len(cv_history)}')
 
-    return scores, cv_history
+    return scores, cv_history, model
 
