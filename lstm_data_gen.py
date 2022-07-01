70b963fa0b955a7ba193b5d3e23b6417348050fc
Miki U <mduryszek@gmail.com>
2022-07-01 13:09:20 +0100

    Changed the infrastucture  of lstm data preprocessing class

--- lstm_data_gen.py
+++ lstm_data_gen.py
@@ -2,54 +2,66 @@ import numpy as np
 import pandas as pd
 from tensorflow.keras.preprocessing import timeseries_dataset_from_array
 
-
 class dataset_generator():
 
-    def __init__(self, df_train, df_val, seq_length, target_column=['close']):
-        self.df_train = df_train
-        self.df_val = df_val
+    def __init__(self, X, cv, seq_length, target_column=['close']):
+        self.X = X
+        self.cv = cv
 
         self.seq_length = seq_length
 
-        self.train_mean = self.df_train.mean()
-        self.train_std = self.df_train.mean()
-
         self.target_column = target_column
+
+        self.fold_data = list()
+
+        self.gen()
+
+    def gen(self):
+        for train, val in self.cv.split(self.X):
+            fold = LSTM_fold_data()
+            fold.train = self.X.iloc[train]
+            fold.val = self.X.iloc[val]
+            fold.train_batch = self.create_lstm_tensor(fold.train_norm)
+            fold.val_batch = self.create_lstm_tensor(fold.val_norm)
+            self.fold_data.append(fold)
+
+    def create_lstm_tensor(self, df):
+        input_data = df.iloc[:-self.seq_length]
+        target = df[self.target_column].iloc[self.seq_length:]
+        return timeseries_dataset_from_array(input_data, target, sequence_length=self.seq_length, shuffle=False)
+
+class LSTM_fold_data():
+    def __init__(self):
+        self.train = None
+        self.val = None
+        self._train_mean = None
+        self._train_std = None
+        self.train_batch = None
+        self.val_batch = None
+
+    @property
+    def train_std(self):
+        if self.train is None:
+            raise Exception("Train data has not been set! Can't compute std.")
+        if self._train_std is None:
+            self._train_std = self.train.std()
+        return self._train_std
+
+    @property
+    def train_mean(self):
+        if self.train is None:
+            raise Exception("Training data has not been set! Can't compute the mean")
+        if self._train_mean is None:
+            self._train_mean = self.train.mean()
+        return self._train_mean
 
     def normalise(self, df):
         return (df - self.train_mean) / self.train_std
 
-    def create_dataset(self, df):
-        data = self.normalise(df)
+    @property
+    def train_norm(self):
+        return self.normalise(self.train)
 
-        input_data = data.iloc[:-self.seq_length]
-        target = data[self.target_column].iloc[self.seq_length:]
-
-        dataset = timeseries_dataset_from_array(input_data, target, sequence_length=self.seq_length, shuffle=False)
-
-        return dataset
-
-    def train(self):
-        return self.create_dataset(self.df_train)
-
-    def val(self):
-        return self.create_dataset(self.df_val)
-
-    def return_numpy(self, df):
-        inputs = np.array([])
-        targets = np.array([])
-        for batch in df:
-            inputs = np.append(inputs, np.array(batch[0]))
-            targets = np.append(targets, np.array(batch[1]))
-
-        return targets
-
-    def plot_target(self):
-        import matplotlib.pyplot as plt
-
-        fig, ax = plt.subplots(figsize=(15, 10))
-
-        targets = self.return_numpy(self.train())
-
-        ax.plot(targets)
-
+    @property
+    def val_norm(self):
+        return self.normalise(self.val)
